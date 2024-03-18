5b8e5f8d613c9e0b7b1c05ac6441e6f828f95424
Ayman Kastali <aymankastali1@gmail.com>
2024-03-18 06:32:50 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
-    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+    path("api/v1/", include("blog.api_urls")),
 ]
