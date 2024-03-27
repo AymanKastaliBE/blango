6a486ed91d2cccf46e403c33707341ab8bd1da9b
Ayman Kastali <aymankastali1@gmail.com>
2024-03-27 10:34:41 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import PostList, PostDetail, UserDetail, PostViewSet
 from rest_framework.authtoken import views
 
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
