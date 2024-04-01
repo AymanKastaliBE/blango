6c3b83b6f0f717147413e088ebe4883513e07c49
Ayman Kastali <aymankastali1@gmail.com>
2024-04-01 10:53:26 +0000

    Finish django-versatileimagefield

--- blango/settings.py
+++ blango/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+    'versatileimagefield',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
@@ -171,6 +172,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- blango/urls.py
+++ blango/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 import blog.views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,7 @@ urlpatterns = [
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("api/v1/", include("blog.api.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
